c21eb927723c8b7a1db4bf534a71849101a6b1cd
mikoli524 <mikobai524@gmail.com>
2023-09-21 03:02:55 +0800

    Build the DashBoard

--- src/components/ProgressCircle.jsx
+++ src/components/ProgressCircle.jsx
@@ -1,7 +1,7 @@
 import { Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 
-const ProgressCircle = ({ progess = "0.75", size = 40 }) => {
+const ProgressCircle = ({ progress = "0.75", size = 40 }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const angle = progress * 360;
@@ -11,7 +11,10 @@ const ProgressCircle = ({ progess = "0.75", size = 40 }) => {
       sx={{
         background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%)
             conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
-        `,
+            ${colors.greenAccent[500]}`,
+        borderRadius: "50%",
+        width: `${size}px`,
+        height: `${size}px`,
       }}
     />
   );
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,22 @@
-import { Box } from "@mui/material";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme.js";
 import Header from "../../components/Header.jsx";
+import { mockTransactions } from "../../data/mockData.js";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadDoneOutlined";
+import EmailIcon from "@mui/icons-material/EmailIcon";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import LineChart from "../../components/LineChart.jsx";
+import BarChart from "../../components/BarChart.jsx";
+import GeographyChart from "../../components/GeographyChart.jsx";
+import StatBox from "../../components/StatBox.jsx";
+import ProgressCircle from "../../components/ProgressCircle.jsx";
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
